81b20b8310471e3dcccec8790aceeb273085b7e6
Ashley Johnson <ashley.duysen@gmail.com>
2025-12-04 10:20:58 +1000

    Added actions to controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,61 @@
 class TasksController < ApplicationController
+  # Runs before the action
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
+
+  # Shows all tasks
   def index
     @tasks = Task.all
   end
 
+  # Shows individual task
   def show
-    @tasks = Task.find(params[:id])
   end
 
+  # Displays form for new task
   def new
-    @task = Task.new(
-      title: params(:title),
-      description: params(:description),
-      completed_staus: params(:completed_staus)
-    )
+    @task = Task.new
   end
 
-  def edit
+  # Processes the form submission
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      # redirect somewhere else on success
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
   end
+
+  # Displays the edit form
+  def edit
+    # puts "@task = #{@task.inspect}"
+  end
+
+  # Process the edit form submission
+  def update
+    if @task.update(task_params)
+      # reditect someone on success
+      redirect_to task_path(@task)
+    else
+      # render again on failure
+      render :edit
+    end
+  end
+
+  # Delete task
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+end
+
+private
+
+def set_task
+  @task = Task.find(params[:id])
+end
+
+def task_params
+  params.require(:task).permit(:title, :description, :completed_status)
 end
